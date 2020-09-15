a5371b2eceb40df5de0c1cd855b1c955f47884a7
Jonathan Pan <62242523+jonopan@users.noreply.github.com>
2020-09-15 17:49:54 +1000

    Add files via upload

---
+++ login_animateEffect/login.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>League Of Legends</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="wallpaper">
+        <!--navigation bar-->
+        <div class="navbar">
+            <img src="images/oceania.png" class="logo">
+            <button type="button"> Sign up</button>
+        </div>
+        <!--content-->
+        <div class="content">
+            <h1>Welcome to Oceania Server</h1>
+            <small>Find our latest trends in the link below</small>
+            <br/>
+            <a href="https://signup.oce.leagueoflegends.com/en/signup/index#/">
+            <button type="button" > Take a tour</button>
+            </a>
+        </div>
+        <!--social & useful link-->
+        <div class="side-bar">
+            <img src="images/menu.png" class="menu">
+            <div class="social-links">
+                <a href="https://www.facebook.com/leagueoflegends/">
+                    <img src="images/fb.png">
+                </a>
+                <a href="https://www.instagram.com/leagueoflegends/?hl=en">
+                    <img src="images/ig.png">
+                </a>
+                <a href="https://twitter.com/LeagueOfLegends?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">
+                    <img src="images/tw.png">
+                </a>
+            </div>
+
+            <div class="useful-links">
+            <a href="https://www.reddit.com/r/summonerschool/comments/4jl64z/a_collection_of_useful_linksressources_for_league/">    
+                <img src="images/share.png">
+            </a>
+            <a href="https://nexus.leagueoflegends.com/en-us/">
+                <img src="images/info.png">
+            </a>
+            </div>
+        </div>
+        <div class="bubbles">
+            <img src="images/red.png">
+            <img src="images/red.png">
+            <img src="images/red.png">
+            <img src="images/blue.png">
+            <img src="images/blue.png">
+            <img src="images/blue.png">
+            <img src="images/blue.png">
+        </div>
+
+    </div>
+
+</body>
+</html>
